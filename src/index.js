c53cfb026e65e0995365cec96d36f21899538c04
Truong Dien <dienrengoku148@gmail.com>
2024-09-17 23:11:29 +0700

    Lab01

--- src/index.js
+++ src/index.js
@@ -11,6 +11,21 @@ root.render(
   </React.StrictMode>
 );
 
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+  }
+}
+
+const person = new Person("John", 25);
+person.sayHello();
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
